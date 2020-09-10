ad2a430e50387a581e351167adfa77bbf59f17bd
Michael Scott <michaelscott@Michaels-MBP.hsd1.pa.comcast.net>
2020-09-10 17:34:51 -0400

    day 3 mvp

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -2,7 +2,6 @@
 Binary search trees are a data structure that enforce an ordering over 
 the data they store. That ordering in turn makes it a lot more efficient 
 at searching for a particular piece of data in the tree. 
-
 This part of the project comprises two days:
 1. Implement the methods `insert`, `contains`, `get_max`, and `for_each`
    on the BSTNode class.
@@ -17,37 +16,191 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        # check if the value is less than the current node's value
+        if value < self.value:
+            # does the current node have a left child?
+            if self.left: 
+                self.left.insert(value)
+            # otherwise, it doesn't have a left child
+            # we can park the new node here
+            else:
+                self.left = BSTNode(value)
+        # otherwise the value is greater or equal to the current node's value
+        else:
+            # does the current node have a right child?
+            if self.right:
+                # if it does, call the right child's `insert` method to repeat the process
+                self.right.insert(value)
+            # otherwise, it doesn't have a right child
+            # we can park the new node here    
+            else:
+                self.right = BSTNode(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        # # does the root have the value?
+        # if target == self.value:
+        #     return True
+        # # does the left child have the value?
+        # if target < self.value:
+        #     if self.left:
+        #        return self.left.contains(target)
+        #     else: 
+        #         return False
+        # # does the right child have the value?
+        # elif target > self.value:
+        #     if self.right:
+        #         return self.right.contains(target)
+        #     else:
+        #         return False
+        # else:
+        #     return False
 
-    # Return the maximum value found in the tree
+        # base case: check self's value to see if it matches the target's
+        if self.value == target:
+            return True
+        # otherwise, we need to go either left or right
+        # compare the target against self's value
+        if target < self.value:
+            # base case: if there's no node here, then the target is not in the tree
+            if not self.left:
+                return False
+            # otherwise, there is a node there
+            # call `contains` on the left child
+            else:
+                return self.left.contains(target)
+        else:
+            # base case: if there's no node here, then the target is not in the tree
+            if not self.right:
+                return False
+            # otherwise there's a node there
+            else: 
+            # call `contains` on the right child
+                return self.right.contains(target)
+
     def get_max(self):
-        pass
+        # while self.right:
+        #     self = self.right
+        # return self.value
+
+        # the max value is always going to be the right-most tree node
+        # Recursive version
+        # if not self.right:
+        #     return self.value
+        # return self.right.get_max()
+
+        # Iterative version
+        current = self
+
+        while current.right:
+            current = current.right
+        
+        # `current` doesn't have a right
+        return current.value
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        # fn(self.value)
+        
+        # if self.left:
+        #     self.left.for_each(fn)
+
+        # if self.right:
+        #     self.right.for_each(fn)
+
+        # Recursive
+        # call fn on self.value
+        # fn(self.value)
+        # # check if self has a left child
+        # if self.left:
+        #     # call `for_each` on the left child, passing in the fn
+        #     self.left.for_each(fn)
+        # # check if right has a right child
+        # if self.right:
+        #     # call `for_each` on the right child, passing in the fn
+        #     self.right.for_each(fn)
+
+        # Depth-First Iterative
+        # how do we achieve the same ordering that recursion gave us for free?
+        # use a stack to achieve the same ordering
+        # stack = []
+        # # add the root node to our stack
+        # stack.append(self)
+
+        # # continue popping from our stack so long as there are nodes in it
+        # while len(stack) > 0:
+        #     current_node = stack.pop()
+
+        #     # check if this node has children
+        #     if current_node.right:
+        #         stack.append(current_node.right)
+        #     if current_node.left:
+        #         stack.append(current_node.left)
+
+        #     fn(current_node.value)
+
+        # Breadth-First Traversal
+        from collections import deque
+
+        q = deque()
+        q.append(self)
+    
+        while len(q) > 0:
+            current_node = q.popleft()
+
+            # check if this node has children
+            if current_node.left:
+                q.append(current_node.left)
+            if current_node.right:
+                q.append(current_node.right)
+            
+            fn(current_node.value)
 
     # Part 2 -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self):
-        pass
+        if not self:
+            return
+        if self.left:
+            self.left.in_order_print()
+        print(self.value)
+        if self.right:
+            self.right.in_order_print()
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self):
-        pass
+        from collections import deque
+
+        q = deque()
+        q.append(self)
+
+        while len(q) > 0:
+            current_node = q.popleft()
+            print(current_node.value)
+            if current_node.left:
+                q.append(current_node.left)
+            if current_node.right:
+                q.append(current_node.right)
+
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self):
-        pass
+        stack = []
+        stack.append(self)
+
+        while len(stack) > 0:
+            current_node = stack.pop()
+            print(current_node.value)
+
+            if current_node.right:
+                stack.append(current_node.right)
+            if current_node.left:
+                stack.append(current_node.left)
 
     # Stretch Goals -------------------------
     # Note: Research may be required
@@ -80,6 +233,6 @@ print("elegant methods")
 print("pre order")
 bst.pre_order_dft()
 print("in order")
-bst.in_order_dft()
+bst.in_order_print()
 print("post order")
-bst.post_order_dft()  
+bst.post_order_dft() 
--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -10,6 +10,11 @@ class ListNode:
         self.value = value
         self.next = next
 
+    def delete(self):
+        if self.prev:
+                self.prev.next = self.next
+        if self.next:
+                self.next.prev = self.prev
 """
 Our doubly-linked list class. It holds references to 
 the list's head and tail nodes.
@@ -76,12 +81,12 @@ class DoublyLinkedList:
         if self.head is None and self.tail is None:
             self.head = new_node
             self.tail = new_node
-        
+
         else:
             current.next = new_node
             new_node.prev = current
             self.tail = new_node
-        
+
         self.length += 1
 
     """
@@ -100,7 +105,7 @@ class DoublyLinkedList:
         else:
             self.head = None
             self.tail = None
-        
+
         self.length -= 1
         return current.value
 
@@ -130,21 +135,42 @@ class DoublyLinkedList:
     """
 
     def delete(self, node):
+        # if self.head is None and self.tail is None:
+        #     return None
+
+        # if self.head == self.tail:
+        #     self.head = None
+        #     self.tail = None
+
+        # if node == self.head:
+        #     self.head = node.next
+        #     self.head.prev = None
+
+        # if node == self.tail:
+        #     self.tail = node.prev
+        #     self.tail.next = None
+
+        # self.length -= 1
+
+        # is there anything to delete?
         if self.head is None and self.tail is None:
-            return None
-        
-        if self.head == self.tail:
+            return  # same as return None
+        # check if there's only one node
+        if node is self.head and node is self.tail:
             self.head = None
             self.tail = None
-        
-        if node == self.head:
+        # check if the node is the head
+        elif node is self.head:
             self.head = node.next
-            self.head.prev = None
-        
-        if node == self.tail:
+            node.delete()
+        # check if the node is the tail
+        elif node is self.tail:
             self.tail = node.prev
-            self.tail.next = None
-
+            node.delete()
+        # otherwise, the node is some node in the middle
+        else:
+            node.delete()
+        # don't forget to decrement the length
         self.length -= 1
     """
     Finds and returns the maximum value of all the nodes 
@@ -152,12 +178,30 @@ class DoublyLinkedList:
     """
 
     def get_max(self):
-        max_val = self.head.value
-        current = self.head
+        # max_val = self.head.value
+        # current = self.head
+
+        # while current is not None:
+        #     if current.value > max_val:
+        #         max_val = current.value
+
+        #     current = current.next
+        # return max_val
+
+        # if the list is empty, return None
+        if self.head is None and self.tail is None:
+            return
+        # keep track of the largest value we've seen so far
+        max_value = self.head.value
+        # traverse the entirety of the linked list
+        current = self.head.next
 
         while current is not None:
-            if current.value > max_val:
-                max_val = current.value
-
+            # if we see a value > the largest value we've seen so far
+            if current.value > max_value:
+                # update the largest value
+                max_value = current.value
+            # update current to point to the next node
             current = current.next
-        return max_val
+        # return the largest value
+        return max_value
